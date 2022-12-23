69137702514a3942d54f700b0d9f9fe2935fa59c
ramadhanialfarizi <ramadhani.alfarizi@gmail.com>
2022-12-23 22:24:12 +0700

    update login pages

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -45,145 +45,149 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     final provider = Provider.of<LoginProvider>(context, listen: false);
     return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                'assets/images/food_icon.png',
-                scale: 1.5,
-              ),
-              SizedBox(
-                height: 19,
-              ),
-              const Text(
-                'Hi, Selamat Datang kembali :)',
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(
-                height: 45,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(right: 25, left: 25),
-                child: Form(
-                  key: formKey,
-                  child: Column(
-                    children: [
-                      // EMAIL
-                      TextFormField(
-                        controller: email,
-                        decoration: InputDecoration(
-                          hintText: 'Email',
-                        ),
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return '* Silahkan memasukkan Email';
-                          }
-                          if (!EmailValidator.validate(value)) {
-                            return '* Masukkan email yang valid';
-                          }
-                          return null;
-                        },
-                      ),
-                      SizedBox(
-                        height: 15,
-                      ),
-                      // PASSWORD
-                      Consumer<LoginProvider>(
-                        builder: (context, value, _) {
-                          return TextFormField(
-                            controller: password,
-                            obscureText: value.obscurePassword,
-                            decoration: InputDecoration(
-                              hintText: 'Password',
-                              suffixIcon: InkWell(
-                                onTap: () => provider.changeObscurePassword(),
-                                child: value.obscurePassword
-                                    ? const Icon(Icons.remove_red_eye_outlined)
-                                    : const Icon(Icons.remove_red_eye),
-                              ),
-                            ),
-                            validator: (value) {
-                              if (value == null || value.isEmpty) {
-                                return '* Silahkan memasukkan Password';
-                              } else if (value.length < 8) {
-                                return '* Password harus minimal 8 karakter';
-                              }
-                              return null;
-                            },
-                          );
-                        },
-                      ),
-                      SizedBox(
-                        height: 45,
-                      ),
-                      SizedBox(
-                        width: double.infinity,
-                        height: 40,
-                        child: ElevatedButton.icon(
-                          icon: const Icon(Icons.account_circle,
-                              color: Colors.white),
-                          label: Text(
-                            'Login',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 15,
-                                color: Colors.white),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/images/food_icon.png',
+                  scale: 1.5,
+                ),
+                SizedBox(
+                  height: 19,
+                ),
+                const Text(
+                  'Hi, Selamat Datang kembali :)',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 45,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 25, left: 25),
+                  child: Form(
+                    key: formKey,
+                    child: Column(
+                      children: [
+                        // EMAIL
+                        TextFormField(
+                          controller: email,
+                          decoration: InputDecoration(
+                            hintText: 'Email',
                           ),
-                          style: ButtonStyle(
-                            backgroundColor: MaterialStateProperty.all<Color>(
-                                Color.fromARGB(194, 249, 7, 108)),
-                          ),
-                          onPressed: () async {
-                            final loginValid = formKey.currentState!.validate();
-
-                            String userEmail = email.text;
-                            String userPassword = password.text;
-
-                            if (loginValid) {
-                              if (FirebaseAuth.instance.currentUser == null) {
-                                try {
-                                  await FirebaseAuth.instance
-                                      .signInWithEmailAndPassword(
-                                          email: userEmail,
-                                          password: userPassword);
-
-                                  loginUser.setBool('login', true);
-                                  loginUser.setString('userEmail', userEmail);
-
-                                  ScaffoldMessenger.of(context).showSnackBar(
-                                    const SnackBar(
-                                      content: Text('Login Berhasil!!'),
-                                      duration: Duration(milliseconds: 800),
-                                    ),
-                                  );
-                                  Navigator.of(context)
-                                      .pushReplacementNamed('/home');
-                                } on FirebaseAuthException catch (e) {
-                                  ScaffoldMessenger.of(context).showSnackBar(
-                                    SnackBar(
-                                      //content: Text(e.message.toString()),
-                                      content: Text('akun tidak ditemukan'),
-                                      duration:
-                                          const Duration(milliseconds: 800),
-                                    ),
-                                  );
-                                }
-                              }
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return '* Silahkan memasukkan Email';
                             }
+                            if (!EmailValidator.validate(value)) {
+                              return '* Masukkan email yang valid';
+                            }
+                            return null;
                           },
                         ),
-                      ),
-                      SizedBox(
-                        height: 15,
-                      ),
-                      signUpButton(),
-                    ],
+                        SizedBox(
+                          height: 15,
+                        ),
+                        // PASSWORD
+                        Consumer<LoginProvider>(
+                          builder: (context, value, _) {
+                            return TextFormField(
+                              controller: password,
+                              obscureText: value.obscurePassword,
+                              decoration: InputDecoration(
+                                hintText: 'Password',
+                                suffixIcon: InkWell(
+                                  onTap: () => provider.changeObscurePassword(),
+                                  child: value.obscurePassword
+                                      ? const Icon(
+                                          Icons.remove_red_eye_outlined)
+                                      : const Icon(Icons.remove_red_eye),
+                                ),
+                              ),
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return '* Silahkan memasukkan Password';
+                                } else if (value.length < 8) {
+                                  return '* Password harus minimal 8 karakter';
+                                }
+                                return null;
+                              },
+                            );
+                          },
+                        ),
+                        SizedBox(
+                          height: 45,
+                        ),
+                        SizedBox(
+                          width: double.infinity,
+                          height: 40,
+                          child: ElevatedButton.icon(
+                            icon: const Icon(Icons.account_circle,
+                                color: Colors.white),
+                            label: Text(
+                              'Login',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15,
+                                  color: Colors.white),
+                            ),
+                            style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all<Color>(
+                                  Color.fromARGB(194, 249, 7, 108)),
+                            ),
+                            onPressed: () async {
+                              final loginValid =
+                                  formKey.currentState!.validate();
+
+                              String userEmail = email.text;
+                              String userPassword = password.text;
+
+                              if (loginValid) {
+                                if (FirebaseAuth.instance.currentUser == null) {
+                                  try {
+                                    await FirebaseAuth.instance
+                                        .signInWithEmailAndPassword(
+                                            email: userEmail,
+                                            password: userPassword);
+
+                                    loginUser.setBool('login', true);
+                                    loginUser.setString('userEmail', userEmail);
+
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      const SnackBar(
+                                        content: Text('Login Berhasil!!'),
+                                        duration: Duration(milliseconds: 800),
+                                      ),
+                                    );
+                                    Navigator.of(context)
+                                        .pushReplacementNamed('/home');
+                                  } on FirebaseAuthException catch (e) {
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      SnackBar(
+                                        //content: Text(e.message.toString()),
+                                        content: Text('akun tidak ditemukan'),
+                                        duration:
+                                            const Duration(milliseconds: 800),
+                                      ),
+                                    );
+                                  }
+                                }
+                              }
+                            },
+                          ),
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        signUpButton(),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
